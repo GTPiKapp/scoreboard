4aaeb777a1341d759b636c3300e255fe7bb201f3
Ricky Barillas <ricky.barillas@gmail.com>
2017-10-17 22:41:53 -0400

    added logger

---
+++ scoreboard_v1/logger.ino
@@ -0,0 +1,38 @@
+void log(String level, String separator, String message)
+{
+    Serial.println("[" + level + "]" + separator + message);
+}
+
+void tabbedLog(String level, String message)
+{
+    log(level, "\t", message);
+}
+
+void printHeader(String message)
+{
+    log("header", "", message);
+}
+
+void error(String message)
+{
+    if (LOG_LEVEL >= ERROR)
+    {
+        tabbedLog("error", message);
+    }
+}
+
+void info(String message)
+{   
+    if (LOG_LEVEL >= INFO)
+    {
+        tabbedLog("info", message);    
+    }
+}
+
+void debug(String message)
+{
+    if (LOG_LEVEL >= DEBUG)
+    {
+        tabbedLog("debug", message);
+    }
+}
