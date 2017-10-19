4b2bc2d12a8167aac9000688f0c5a4bca622b0cd
Ricky Barillas <ricky.barillas@gmail.com>
2017-10-18 23:08:16 -0400

    moved screen related stuff together

--- scoreboard/time.ino
+++ scoreboard/time.ino
@@ -9,45 +9,6 @@ void reset_time()
   time = 300;
 }
 
-void print_screen()
-{
-  double frac, int1, int2, int3, temp;
-
-  // Clear Screen
-  matrix.fillScreen(0);
-  color = white;
-  drawTeams();
-  drawScore();
-  drawTemp();
-  color = red;
-  
-  // Minutes digit                    ex: num = 345
-  temp = (double) time / 60; //                   temp=5.75
-  frac = modf(temp, &int1); //     frac=.75, intpart=5
-  
-  // Tens digit
-  temp = (time - 60 * int1)/10; //    temp= 4.5
-  frac = modf(temp, &int2); //     frac=.5, intpart=4
-  
-  // Seconds digit
-  temp = frac * 10; //                  temp=5
-  int3 = temp + 0.5; // int conversion inside drawDigit sees a floating 5.00 as 4.993882288134081 or whatever, for some reason. Add arbitrary 0.5 to ensure it's above truncation point
-
-  setCurs(4);
-  drawDigit(int1);
-  // colon 
-  matrix.drawPixel(c + 9, r + 3, color);
-  matrix.drawPixel(c + 9, r + 4, color);
-  matrix.drawPixel(c + 9, r + 8, color);
-  matrix.drawPixel(c + 9, r + 9, color);
-  setCurs(5);
-  drawDigit(int2);
-  setCurs(6);
-  drawDigit(int3);
-  
-  matrix.swapBuffers(false);
-}
-
 int increment_time()
 {
   if(time >= 420)
@@ -210,7 +171,7 @@ int count_down_time()
       {
         getTemp();
       }
-      
+
       print_screen();
       
       if (time <= 3)
